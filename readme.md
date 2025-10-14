e408c146feb1c2c4bc430baabf22c03326e2d681
Parad0x763 <db726825@gmail.com>
2025-10-13 21:15:28 -0400

    Added the Quick Start -> ECS code for easy reference on how to use the Entity Component System from the Bevy engine

--- readme.md
+++ readme.md
@@ -1,6 +1,12 @@
 # Asteroids Built in Rust Using Bevy
 
+- The goal of this project is to create the classic Asteroids game using Rust and to learn the Bevy Game Engine
+
 ## Bevy
 
 - [bevy-engine](https://bevy.org/)
 - `cargo add bevy # Adds bevy as a package`
+
+### ECS Quick Start
+
+- [Quick-Start/ECS](https://bevy.org/learn/quick-start/getting-started/ecs/)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,51 @@
-use bevy::app::App;
+use bevy::app::{App, Startup, Update};
+use bevy::ecs::query::With;
+use bevy::ecs::schedule::IntoScheduleConfigs as _;
+use bevy::ecs::system::{Commands, Query};
+use bevy::ecs::{component::Component};
+
+
+fn hello_world() {
+    println!("hello world");
+}
+
+
+#[derive(Component)]
+struct Person;
+
+
+#[derive(Component)]
+struct Name(String);
+
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Dennis Ritchey".to_string())));
+    commands.spawn((Person, Name("Ken Thomson".to_string())));
+    commands.spawn((Person, Name("Graydon Hoare".to_string())));
+}
+
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}", name.0);
+    }
+}
+
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Graydon Hoare" {
+            name.0 = "Graydon Hoare: Rust Creator".to_string();
+            break; // just updating this one name
+        }
+    }
+}
 
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_systems(Startup, add_people)
+        // using .chain() forces the systems to run in exactly the order listed
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .run();
 }
